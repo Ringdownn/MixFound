db7a0a0761fcac5bcdc5fe2f488859fdea7015de
Ringdownn <3227146620@qq.com>
2026-03-16 01:03:06 +0800

    model doc

--- searcher/model/doc.go
+++ searcher/model/doc.go
@@ -1,1 +1,33 @@
 package model
+
+type IndexDoc struct {
+	Id       uint32                 `json:"id,omitempty"`
+	Text     string                 `json:"text,omitempty"`
+	Document map[string]interface{} `json:"document,omitempty"`
+}
+
+type StorageIndexDoc struct {
+	*IndexDoc
+	Keys []string `json:"keys,omitempty"`
+}
+
+type ResponseDoc struct {
+	IndexDoc
+	OriginalText string   `json:"originalText,omitempty"`
+	Score        int      `json:"score,omitempty"`
+	Keys         []string `json:"keys,omitempty"`
+}
+
+type ResponseDocsSort []ResponseDoc
+
+func (r ResponseDocsSort) Len() int {
+	return len(r)
+}
+
+func (r ResponseDocsSort) Less(i, j int) bool {
+	return r[i].Score < r[j].Score
+}
+
+func (r ResponseDocsSort) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
